4b2352a439a18dbc7157daf882d1a82e18d4b98d
hyemin Kim <hmkim199@gmail.com>
2020-01-08 21:22:20 +0900

    Initial Commit

---
+++ Dalmuti/src/Card.java
@@ -0,0 +1,8 @@
+
+public class Card {
+	private int number;
+	
+	public Card(int number) {
+		this.number = number;
+	}
+}
--- Dalmuti/src/Dalmuti.java
+++ Dalmuti/src/Dalmuti.java
@@ -3,6 +3,23 @@ public class Dalmuti {
 
 	public Dalmuti() {
 		System.out.println("Welcome to Dalmuti.");
+		
+		//카드 생성
+		Card card[] = new Card[57];
+		int index = 0;
+		for(int i = 1; i <= 10; i++) {			
+			for(int j = 0; j < i; j++) {
+				
+				card[index] = new Card(i);
+				//System.out.printf("%d,%d/", index,i);
+				index++;	
+			}
+			System.out.println();
+		}
+		card[55] = new Card(13);
+		card[56] = new Card(13);
+		
+		
 	}
 	
 	public static void main(String[] args) {

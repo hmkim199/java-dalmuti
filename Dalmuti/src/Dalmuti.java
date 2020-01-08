4b16d685ebc07c36507d12ee0ec84c32aa60e12a
Susan Kim <gfsusan@naver.com>
2020-01-08 21:27:02 +0900

    Renamed variable

--- Dalmuti/src/Dalmuti.java
+++ Dalmuti/src/Dalmuti.java
@@ -5,19 +5,19 @@ public class Dalmuti {
 		System.out.println("Welcome to Dalmuti.");
 		
 		//카드 생성
-		Card card[] = new Card[57];
+		Card cards[] = new Card[57];
 		int index = 0;
 		for(int i = 1; i <= 10; i++) {			
 			for(int j = 0; j < i; j++) {
 				
-				card[index] = new Card(i);
+				cards[index] = new Card(i);
 				//System.out.printf("%d,%d/", index,i);
 				index++;	
 			}
 			System.out.println();
 		}
-		card[55] = new Card(13);
-		card[56] = new Card(13);
+		cards[55] = new Card(13);
+		cards[56] = new Card(13);
 		
 		
 	}

cf277c6713faab2721c3d49377bfee4059a3fa18
hyemin Kim <hmkim199@gmail.com>
2020-02-17 21:33:51 +0900

    카드 gui 추가ㅏ하고싶었다..

--- Dalmuti/src/MainView.java
+++ Dalmuti/src/MainView.java
@@ -3,7 +3,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,15 +27,50 @@ public class MainView extends JFrame {
 		
 		JLabel rankLabel = new JLabel("랭크");
 		JLabel nameLabel = new JLabel("이름");
-		JLabel cardLabel = new JLabel();
 		
-		ImageIcon cardImage = new ImageIcon();
+		ImageIcon cardImage = new ImageIcon("res/card1.png");
+		
+		JLabel[] cardLabels = new JLabel[15];
+		for (int i = 0; i < 15; i++) {
+			JLabel cardLabel = cardLabels[i];
+			cardLabel = new JLabel("이미");
+//			cardLabel.setSize(60,80);
+			cardLabel.setPreferredSize(new Dimension(60,80));
+			
+			
+			try {
+				String imagePath = "res/1.png";
+				Image image = ImageIO.read(new File(imagePath));
+				Image scaledImage = image.getScaledInstance(60, 80, 0);
+				ImageIcon imageIcon = new ImageIcon(scaledImage);
+				cardLabel.setIcon(imageIcon);
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		JPanel cardPanel = new JPanel();
+		for (int i = 0; i < 15; i++) {
+			cardPanel.add(cardLabels[i]);
+		}
+		
+		
+		cardPanel.setLayout(new GridLayout(1,15));		
+//		cardPanel.setPreferredSize(new Dimension(800,300));
+		cardPanel.setBackground(Color.pink);
+		cardPanel.setOpaque(true);
+		
+		
+		JPanel playerInfoPanel = new JPanel();
+		playerInfoPanel.setLayout(new GridLayout(2,1));
+		playerInfoPanel.setBackground(Color.lightGray);
+		playerInfoPanel.add(rankLabel);
+		playerInfoPanel.add(nameLabel);
 		
 		JPanel playerPanel = new JPanel();
-		playerPanel.setLayout(new GridLayout(2,1));
-		playerPanel.setBackground(Color.lightGray);
-		playerPanel.add(rankLabel);
-		playerPanel.add(nameLabel);
+		playerPanel.setLayout(new BorderLayout());
+		playerPanel.add(cardPanel, BorderLayout.CENTER);
+		playerPanel.add(playerInfoPanel, BorderLayout.SOUTH);
 		
 		JPanel boardPanel = new JPanel();
 		boardPanel.setBackground(new Color(255,235,96));
